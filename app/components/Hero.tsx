51b1f9856c462da233f67dc767be1155a2d52d6c
Papan Banik <papanbanik58@gmail.com>
2026-04-24 21:41:41 +0600

    modify animation on hero

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -22,22 +22,18 @@ const Hero = () => {
          </motion.div> 
       </div>
    <div className="z-50"><Navbar/></div>
-   <div className="flex flex-col items-center justify-center text-center text-white flex-1 px-4 sm:pt-5">
+   <motion.div 
+        initial = {{opacity:0}}
+        animate={{opacity:1}}
+        transition={{duration:2}}
+       className="flex flex-col items-center justify-center text-center text-white flex-1 px-4 sm:pt-5">
        <div className="bg-[#FAB326] flex items-center gap-2 rounded-full px-4 py-2 mb-4 max-sm:mt-10">
           <Image src="/earth-logo.png" width={20} height={20} alt="earth" />
           <span>Explore the world</span>
         </div>
-        <motion.h2 
-        initial = {{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:2}}
-        className="text-2xl md:text-5xl font mb-4 max-sm:mt-">Your Gateway to Extraordinary Adventures</motion.h2>
-        <motion.p
-        initial = {{opacity:0}}
-        animate={{opacity:1}}
-        transition={{duration:2}}
-        className="text-sm md:text-base max-w-xl">Pack your bags and let Travila redefine your travel experience.</motion.p>
-     </div>
+        <h2 className="text-2xl md:text-5xl font mb-4 max-sm:mt-">Your Gateway to Extraordinary Adventures</h2>
+        <p className="text-sm md:text-base max-w-xl">Pack your bags and let Travila redefine your travel experience.</p>
+     </motion.div>
      <div className="relative z-50 -mt-16 px-4"><HeroCard/></div>
     </div>
   );
